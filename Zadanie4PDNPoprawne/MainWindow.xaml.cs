5afe184189231e60a4d60f0afa1f648e1d231fa3
AlicjaZiebinska <alicjaziebinska95@gmail.com>
2024-05-31 20:13:09 +0200

    first commit

---
+++ Zadanie4PDNPoprawne/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie4PDNPoprawne
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var index = DataGridMain.SelectedIndex;
+            var provider = new XmlDataProvider();
+            provider.Source = new Uri("Pliki\\Input.xml", UriKind.Relative);
+            provider.XPath = $"Lista/Kategoria[{index+1}]/Podkategoria";
+
+            var oknoKategorii = new OknoKategorii(index);
+            oknoKategorii.GridKategorii.DataContext = provider;
+            oknoKategorii.Show();
+        }
+    }
+}
---
+++ Zadanie4PDNPoprawne/OknoKategorii.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Zadanie4PDNPoprawne
+{
+    /// <summary>
+    /// Logika interakcji dla klasy OknoKategorii.xaml
+    /// </summary>
+    public partial class OknoKategorii : Window
+    {
+        public OknoKategorii(int index)
+        {
+            InitializeComponent();
+            mainIndex = index;
+        }
+
+        private readonly int mainIndex;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var index = DataGridKat.SelectedIndex;
+            var provider = new XmlDataProvider();
+            provider.Source = new Uri("Pliki\\Input.xml", UriKind.Relative);
+            provider.XPath = $"Lista/Kategoria[{mainIndex + 1}]/Podkategoria[{index+1}]/Model";
+
+            var oknoModelu= new OknoModelu();
+            oknoModelu.GridKategorii.DataContext = provider;
+            oknoModelu.Show();
+        }
+    }
+}
